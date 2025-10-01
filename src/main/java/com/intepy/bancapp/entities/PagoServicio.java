e27f056592552eae0df65ebac65ee5990f4a14ac
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-01 11:58:16 -0300

    Add JSON back reference on PagoServicio
    This type of reference: cuenta-pagos.

--- src/main/java/com/intepy/bancapp/entities/PagoServicio.java
+++ src/main/java/com/intepy/bancapp/entities/PagoServicio.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +34,7 @@ public class PagoServicio {
     @Setter
     @ManyToOne
     @JoinColumn(name = "cuenta_id")
+    @JsonBackReference("cuenta-pagos")
     private Cuenta cuenta;
 
     @Getter
