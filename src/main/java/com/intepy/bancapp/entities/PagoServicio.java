6edac1d9dd714117e9f4670d283bf1a205faa126
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-01 17:55:40 -0300

    Add EntityListener on PagoServicio

--- src/main/java/com/intepy/bancapp/entities/PagoServicio.java
+++ src/main/java/com/intepy/bancapp/entities/PagoServicio.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,6 +21,7 @@ import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class PagoServicio {
 
     @Getter
