3fc777feaede2ecacbb8c297e682a02802614ec4
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-01 17:46:53 -0300

    Add EntityListeners on Deposito and Transferencia

--- src/main/java/com/intepy/bancapp/entities/Transferencia.java
+++ src/main/java/com/intepy/bancapp/entities/Transferencia.java
@@ -4,8 +4,10 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,6 +21,7 @@ import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Transferencia {
 
     @Getter
