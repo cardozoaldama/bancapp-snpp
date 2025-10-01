4bc65835f5df94e775d7f13c3d440dd7de5eb3f4
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-01 17:45:57 -0300

    Add JPA auditing config class

---
+++ src/main/java/com/intepy/bancapp/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+// src/main/java/com/intepy/bancapp/config/JpaAuditingConfig.java
+package com.intepy.bancapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
