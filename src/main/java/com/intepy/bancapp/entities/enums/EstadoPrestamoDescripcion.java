2c172f3f7dd230addcfd49ec9e803bd12d57b000
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-15 17:50:25 -0300

    Delete enum Spanish files

--- src/main/java/com/intepy/bancapp/entities/enums/EstadoPrestamoDescripcion.java
+++
@@ -1,7 +0,0 @@
-package com.intepy.bancapp.entities.enums;
-
-public enum EstadoPrestamoDescripcion {
-    APROBADO,
-    PENDIENTE,
-    RECHAZADO
-}
--- src/main/java/com/intepy/bancapp/entities/enums/TipoCuentaBasica.java
+++
@@ -1,6 +0,0 @@
-package com.intepy.bancapp.entities.enums;
-
-public enum TipoCuentaBasica {
-    A_LA_VISTA,
-    CORRIENTE
-}
