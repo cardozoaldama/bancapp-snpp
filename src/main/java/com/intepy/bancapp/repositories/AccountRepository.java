a29a2c6aeffb77f427a5922fb80f2e0a2cfbd71e
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-15 21:21:16 -0300

    Add findByNumber query method to AccountRepository
    Add custom query method to find accounts by account number. This
    supports the new TransferRequestDTO functionality where transfers
    are created using account numbers instead of entity IDs.
    
    Method signature:
    Optional<Account> findByNumber(String number)
    
    Spring Data JPA will automatically implement this query method.

--- src/main/java/com/intepy/bancapp/repositories/AccountRepository.java
+++ src/main/java/com/intepy/bancapp/repositories/AccountRepository.java
@@ -1,9 +1,11 @@
 package com.intepy.bancapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.intepy.bancapp.entities.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    Optional<Account> findByNumber(String number);
 }
